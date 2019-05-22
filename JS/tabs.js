7684d9c7c584196719aa7d50b084e9cafddf002b
Philip Johnson <phil7j@gmail.com>
2019-05-22 15:54:06 -0700

    made site responsive, added additional header styling and abou-page styling

--- JS/tabs.js
+++ JS/tabs.js
@@ -66,15 +66,8 @@ class TabLink {
 
   }
 
-  /* START HERE:
+//    START HERE:
 
-  - Select all classes named ".tab" and assign that value to the tabs variable
-
-  - With your selection in place, now chain a .forEach() method onto the tabs variable to iterate over the DOM NodeList
-
-  - In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
-
-  */
   let tabs = document.querySelectorAll('.tabs-link');
 
   tabs.forEach( tab => {
